450779a6402febd4b3fcc733ad9b61eefbd305ab
mdmhrz <razufreelence@gmail.com>
2025-03-15 21:39:21 +0600

    Get words details by id done

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-// For loading Dynamic Buttons
+// For loading Dynamic Buttons 3-29 line
 const loadLevels = async () => {
     try {
         const response = await fetch('https://openapi.programming-hero.com/api/levels/all');
@@ -9,6 +9,7 @@ const loadLevels = async () => {
         console.error('Error fetching levels:', error);
     }
 };
+
 
 const showLessonButtons = (buttons) => {
     const lessonContainer = document.getElementById('lessonContainer');
@@ -29,7 +30,7 @@ const showLessonButtons = (buttons) => {
 
 
 
-// For Loading Dynamic words
+// For Loading Dynamic words 33 to 80 line
 const loadWords = async (id) => {
     try {
         const response = await fetch(`https://openapi.programming-hero.com/api/level/${id}`);
@@ -42,12 +43,20 @@ const loadWords = async (id) => {
 };
 
 const showWords = (words) => {
+    // To show warning box
     if (words.length === 0) {
         showElementsByID('warning-dialogue-display')
     }
+    else {
+        hideElementsByID('warning-dialogue-display')
+    }
+
+    // To show words in container by making loop
     const wordsContainer = document.getElementById('words-container');
     wordsContainer.innerHTML = '';
+
     words.forEach((word) => {
+        // console.log(word)
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="bg-gray-100 rounded-md shadow-md p-5 text-center hover:bg-violet-100">
@@ -57,7 +66,7 @@ const showWords = (words) => {
                     <h1 class="text-3xl font-bold mt-3">${word.meaning} / ${word.pronunciation}</h1>
                 </div>
                 <div class="flex items-center justify-between">
-                    <div onclick="my_modal_3.showModal()" class="w-12 h-12 cursor-pointer hover:bg-violet-300 rounded-sm bg-violet-200 flex items-center justify-center">
+                    <div onclick="loadWordDetails('${word.id}')" class="w-12 h-12 cursor-pointer hover:bg-violet-300 rounded-sm bg-violet-200 flex items-center justify-center">
                         <i class="fa-solid fa-circle-info"></i>
                     </div>
                     <div class="w-12 h-12 cursor-pointer hover:bg-violet-300 rounded-sm bg-violet-200 flex items-center justify-center">
@@ -68,23 +77,62 @@ const showWords = (words) => {
         `;
         wordsContainer.appendChild(div);
 
-        const modalContainersectoin = document.getElementById('modal-contaier');
-        const newdiv = document.createElement('dialog');
-        newdiv.classList.add('modal')
-        newdiv.id.add('my_modal_3')
-        newdiv.innerHTML = `
-            <div class="modal-box">
-                <form method="dialog">
-                    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <h3 class="text-lg font-bold">Hello!</h3>
-                <p class="py-4">Press ESC key or click on ✕ button to close</p>
-            </div>
-        `
-        modalContainersectoin.appendChild(newdiv)
-
     });
 };
+
+// my_modal_3.showModal()
+
+const loadWordDetails = async (id) => {
+    try {
+        const response = await fetch(`https://openapi.programming-hero.com/api/word/${id}`);
+        const data = await response.json();
+        showWordDetails(data.data)
+
+    } catch (error) {
+        console.error('Error fetching words:', error);
+    }
+}
+
+
+const showWordDetails = (data) => {
+    console.log(data.word)
+    const modalDataContainer = document.getElementById('modalDataContainer');
+    modalDataContainer.innerHTML = ''
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h1 class="text-3xl font-bold mb-8" >${data.word} (<span><i class="fa-solid fa-microphone-lines"></i></span> : <span>${data.pronunciation}</span> )</h1>
+        <p class="font-bold mb-2">Meaning</p>
+        <p class="text-2xl mb-6">${data.meaning}</p>
+        <p class="font-bold mb-2">Example</p>
+        <p class="mb-6">${data.sentence}</p>
+        <p class="font-semibold text-[16px] mb-2">সমার্থক শব্দগুলো</p>
+        <div class="flex gap-3 flex-wrap">
+            <button class="bg-violet-100 btn ">${data.synonyms[0]}</button>
+            <button class="bg-violet-100 btn ">${data.synonyms[1]}</button>
+            <button class="bg-violet-100 btn ">${data.synonyms[2]}</button>
+        </div>
+    `
+    modalDataContainer.appendChild(div)
+    my_modal_3.showModal()
+
+}
+
+
+
+
+
+/*
+
+
+
+
+
+*/
+
+
+
+
+
 
 
 
